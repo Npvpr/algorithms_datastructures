c934ceb7d3b78bf31d9e5475027cd871e71bc717
Npvpr <nlmnever2@gmail.com>
2025-02-08 21:19:10 +0000

    Palindrome and ContainerMostWater

---
+++ algorithms/Two Pointers/Palindrome.java
@@ -0,0 +1,31 @@
+public class Palindrome {
+    // skip non-alphanumeric characters
+    public static void main(String[] args) {
+        System.out.println(solution("") == true ? "Correct" : "Error");
+        System.out.println(solution("a") == true ? "Correct" : "Error");
+        System.out.println(solution(",:") == true ? "Correct" : "Error");
+        System.out.println(solution("AbBa") == true ? "Correct" : "Error");
+        System.out.println(solution("defeda") == false ? "Correct" : "Error");
+    }
+
+    public static boolean solution(String inputStr) {
+        int left = 0, right = inputStr.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isAlphabetic(inputStr.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isAlphabetic(inputStr.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(inputStr.charAt(left)) != Character.toLowerCase(inputStr.charAt(right))) {
+                // System.out.println("Not equal: " + inputStr.charAt(left) + " vs " + inputStr.charAt(right));
+                return false;
+            }
+            // System.out.println("Equal: " + inputStr.charAt(left) + " vs " + inputStr.charAt(right));
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
